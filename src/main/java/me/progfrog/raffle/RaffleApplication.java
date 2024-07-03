2ce596c15d4a39bcc7c3c3952da1689c6a4f96c9
happyprogfrog <happyprogfrog@gmail.com>
2024-07-04 08:54:41 +0900

    프로젝트 생성

---
+++ src/main/java/me/progfrog/raffle/RaffleApplication.java
@@ -0,0 +1,13 @@
+package me.progfrog.raffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaffleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/progfrog/raffle/RaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.raffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
